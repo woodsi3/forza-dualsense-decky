5abf0252a98fe57a5646426e4b46eb8aaf8d37fb
Oli Woods <woods.ollie@gmail.com>
2026-07-15 19:57:07 +0100

    Release v1.0.0

--- main.py
+++ main.py
@@ -555,7 +555,7 @@ class Plugin:
         self.settings_lock = asyncio.Lock()
         await self._ensure_settings()
         self.startup_task = self.loop.create_task(self._startup_backend())
-        decky.logger.info("Forza DualSense Haptics v0.5.5 loaded")
+        decky.logger.info("Forza DualSense Haptics v1.0.0 loaded")
 
     async def _unload(self):
         if self.startup_task is not None and not self.startup_task.done():
